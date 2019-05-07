63f8b6f0274beefec4c4fe42eccb9e5637cf8ef8
Vinh Le <vinh@vertics.co>
2019-05-07 20:19:36 +0300

    feat: add global middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
 
 export const app = express()
+
+// Global middlewares
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
 
 app.get('/', (req, res, next) => {
   res.json('Welcome to Awesome Node Boilerplate')
