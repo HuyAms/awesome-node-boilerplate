72833177678d805d75f181ae388ef9e5c5d831a5
HuyTrinh <dinhhuyams@gmail.com>
2019-05-21 07:32:27 +0300

    chore: update swagger

--- src/resources/auth/auth.router.ts
+++ src/resources/auth/auth.router.ts
@@ -2,27 +2,31 @@ import {Router} from 'express'
 import * as authController from './auth.controller'
 import {validateSignIn, validateSignUp} from './auth.validator'
 
-const router = Router()
-
 /**
  * @swagger
  * tags:
  * - name: Authentication
  */
+const router = Router()
 
 /**
  * @swagger
- * /signup:
+ * /auth/signup:
  *   post:
  *    tags:
  *    - Authentication
  *    summary: Sign up user
- *    parameters:
- *    - in: body
- *    required: true
  */
 router.route('/signup').post(validateSignUp(), authController.signup)
 
+/**
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *    tags:
+ *    - Authentication
+ *    summary: Sign in user
+ */
 router.route('/signin').post(validateSignIn(), authController.signin)
 
 export default router
--- src/resources/user/user.router.ts
+++ src/resources/user/user.router.ts
@@ -3,16 +3,46 @@ import * as userController from './user.controller'
 import {Permission, protect} from '../../middlewares/permission'
 import {validateUpdateUser} from './user.validator'
 
+/**
+ * @swagger
+ * tags:
+ * - name: User
+ */
 const router = Router()
 
 const writeUser = protect([Permission.UserWrite])
 const readUser = protect([Permission.UserRead])
 
+/**
+ * @swagger
+ * /api/user:
+ *   get:
+ *    tags:
+ *    - User
+ *    summary: Get all users
+ */
 router.route('/').get(readUser, userController.getMany)
 
+/**
+ * @swagger
+ * /api/user/me:
+ *   get:
+ *    tags:
+ *    - User
+ *    summary: Get my profile
+ */
 router.route('/me').get(readUser, userController.getMe)
 
 router.param('id', userController.params)
+
+/**
+ * @swagger
+ * /api/user/:id:
+ *   get:
+ *    tags:
+ *    - User
+ *    summary: Get one user
+ */
 router
 	.route('/:id')
 	.get(readUser, userController.getOne)
