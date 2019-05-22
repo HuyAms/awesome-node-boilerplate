8158aaf550c23050025b03a78ed56d13716ff01d
HuyTrinh <dinhhuyams@gmail.com>
2019-05-22 07:25:55 +0300

    refactor: log and email message of reset password

--- src/resources/auth/auth.controller.ts
+++ src/resources/auth/auth.controller.ts
@@ -60,10 +60,15 @@ export const signin: RequestHandler = (req, res, next) => {
  * Save a reset password token and reset password expire to user model
  * Send user a link that has the reset password token
  *
+ * @param req
+ * @param res
+ * @param next
  */
 export const forgotPassword: RequestHandler = async (req, res, next) => {
 	// Check if email that user submitted belongs to an user
 	const {email} = req.body
+	logger.debug('Forgot password: ', email)
+
 	try {
 		let user = await findUserWithEmail(email)
 		if (!user) {
@@ -72,11 +77,10 @@ export const forgotPassword: RequestHandler = async (req, res, next) => {
 		}
 		// Create reset password token
 		const resetPasswordToken = uuidv4()
-		// Set expired time to be 1 hour
-		const resetPasswordExp = Date.now() + 3600000
-		// Save them to user object
+		const resetPasswordExp = Date.now() + 3600000 //1 hour
 		user.resetPasswordToken = resetPasswordToken
 		user.resetPasswordExp = resetPasswordExp
+
 		// Save user to the database
 		await saveUser(user)
 		// Send an email to user, containing the reset password token
@@ -88,11 +92,15 @@ export const forgotPassword: RequestHandler = async (req, res, next) => {
 			from: config.mailSender,
 			to: user.email,
 			subject: 'Reset password',
-			text: `Please click this link to reset password ${resetUrl}`,
+			text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
+          Please click on the following link, or paste this into your browser to complete the process:\n\n
+          ${resetUrl}
+          If you did not request this, please ignore this email and your password will remain unchanged.\n`,
 		}
 
 		await sendEmail(message)
-		return res.status(201).send({message: 'Please check your email'})
+		logger.debug('Send reset password link to email: ', user.email)
+		return res.json({message: 'Please check your email'})
 	} catch (error) {
 		next(error)
 	}
@@ -103,27 +111,34 @@ export const forgotPassword: RequestHandler = async (req, res, next) => {
  * Verify reset password token from request param
  * Save new user password and clear reset password token & expire
  *
+ * @param req
+ * @param res
+ * @param next
  */
 export const resetPassword: RequestHandler = async (req, res, next) => {
 	// Check if the token in req params match with an user in db
 	const {resetToken} = req.params
 	try {
 		const user = await findUserWithToken(resetToken)
-		logger.debug('User', user)
+		logger.debug('Reset password of user: %o', user)
+
 		if (!user) {
 			next(apiError.notFound('Cannot find user with provided token'))
 		}
+
 		// Check if expire time is over
 		const resetPasswordExp = user.resetPasswordExp
 		if (Date.now() > resetPasswordExp) {
-			next(apiError.notFound('Token is already expired'))
+			next(apiError.badRequest('Token is already expired'))
 		}
+
 		// Check if user sends a password that is exact to be old one
 		const {password} = req.body
 		const oldPassword = user.password
 		if (password === oldPassword) {
 			next(apiError.badRequest('New password should not match with old one'))
 		}
+
 		// Save new user passsword
 		// and remove reset token and expired time
 		user.password = password
@@ -135,7 +150,7 @@ export const resetPassword: RequestHandler = async (req, res, next) => {
 		const successMessage = {
 			from: config.mailSender,
 			to: user.email,
-			subject: 'You password has been resetted',
+			subject: 'You password has been reset',
 			text: `This is a confirmation message for account ${
 				user.email
 			}. Your password has just been changed`,
@@ -145,7 +160,7 @@ export const resetPassword: RequestHandler = async (req, res, next) => {
 
 		return res
 			.status(200)
-			.send({message: 'Password has been successfully resetted'})
+			.send({message: 'Password has been successfully reset'})
 	} catch (error) {
 		next(apiError.notFound(error))
 	}
