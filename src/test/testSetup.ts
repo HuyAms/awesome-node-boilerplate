d965eebd4a2eda5f0e6dd4dc35262f1b71a80ee1
HuyTrinh <huy@makehelsinki.com>
2019-06-02 13:04:51 +0300

    refactor: testSetup, connect to different to test db each test

--- src/test/testSetup.ts
+++ src/test/testSetup.ts
@@ -1,15 +1,60 @@
-import mongoose from 'mongoose'
-import {clearDB} from './utils/db'
+import mongoose, {Collection} from 'mongoose'
+import uuid from 'uuid/v1'
+import _ from 'lodash'
 import connectDb from '../services/db'
+import config from '../config'
+
+const databaseUrl = config.dbUrl
+
+const remove = async (collection: Collection) => {
+	try {
+		await collection.deleteMany({})
+		return Promise.resolve()
+	} catch (e) {
+		return Promise.reject(e)
+	}
+}
 
 beforeAll(() => {
 	return connectDb()
 })
 
-beforeEach(() => {
-	return clearDB()
+beforeEach(async done => {
+	const db = uuid()
+
+	const clearDB = () => {
+		return Promise.all(_.map(mongoose.connection.collections, c => remove(c)))
+	}
+
+	/*
+	If the mongoose connection is closed,
+	start it up using the test url and database name
+	*/
+	const disconnectedState = 0
+	if (mongoose.connection.readyState === disconnectedState) {
+		try {
+			await mongoose.connect(databaseUrl + db, {
+				useNewUrlParser: true,
+				autoIndex: true,
+			})
+			await clearDB()
+		} catch (e) {
+			console.log('connection error')
+			console.error(e)
+			throw e
+		}
+	} else {
+		await clearDB()
+	}
+	done()
 })
 
-afterAll(() => {
-	return mongoose.disconnect()
+afterEach(async done => {
+	await mongoose.connection.db.dropDatabase()
+	await mongoose.disconnect()
+	done()
+})
+
+afterAll(done => {
+	return done()
 })
--- src/test/utils/db.ts
+++ src/test/utils/db.ts
@@ -1,21 +1,6 @@
-import _ from 'lodash'
-import mongoose, {Collection} from 'mongoose'
 import {UserRole, UserStatus} from '../../resources/user/user.interface'
 import UserModel, {UserDocument} from '../../resources/user/user.model'
 import {createMockUser} from './mock'
-
-const remove = async (collection: Collection) => {
-	try {
-		await collection.deleteMany({})
-		return Promise.resolve()
-	} catch (e) {
-		return Promise.reject(e)
-	}
-}
-
-export const clearDB = () => {
-	return Promise.all(_.map(mongoose.connection.collections, c => remove(c)))
-}
 
 export const addUser = (
 	role: UserRole = UserRole.User,
