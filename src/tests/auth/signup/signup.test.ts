a23c3e6e71d872a01abf177f2d66fdcbc9731684
Vinh Le <vinh@vertics.co>
2019-06-25 18:10:14 +0300

    feat: add signup test suite

--- src/tests/auth/signup/signup.test.ts
+++ src/tests/auth/signup/signup.test.ts
@@ -2,15 +2,18 @@ import httpStatus from 'http-status'
 import {addUser} from '../../utils/db'
 import {apiRequest} from '../../utils/common'
 import {createMockUser} from '../../utils/mock'
+import {ErrorCode} from '../../../utils/apiError'
 
 import {User} from '../../../resources/user/user.interface'
 
 describe('[SIGNUP API]', () => {
 	let dummyUser: User
+	let existingUser: User
 
 	beforeEach(async () => {
 		dummyUser = createMockUser()
-		await addUser(createMockUser())
+		existingUser = createMockUser()
+		await addUser(existingUser)
 	})
 
 	describe('POST /auth/signup', () => {
@@ -19,7 +22,7 @@ describe('[SIGNUP API]', () => {
 			const {email, passport, firstName, lastName} = dummyUser
 			const {password} = passport
 
-			// Actoin
+			// Action
 			const result = await apiRequest
 				.post('/auth/signup')
 				.send({email, passport, password, firstName, lastName})
@@ -27,6 +30,52 @@ describe('[SIGNUP API]', () => {
 			// Expect
 			expect(result.status).toEqual(httpStatus.OK)
 			expect(result.body.data.token)
+		})
+
+		it('Should return 400 with email not unique error code when signin up with existing email', async () => {
+			// Arrange
+			const {email, passport, firstName, lastName} = existingUser
+			const {password} = passport
+
+			// Action
+			const result = await apiRequest
+				.post('/auth/signup')
+				.send({email, passport, password, firstName, lastName})
+
+			const {status, errorCode} = result.body
+			// Expect
+			expect(status).toEqual(httpStatus.BAD_REQUEST)
+			expect(errorCode).toEqual(ErrorCode.emailNotUnique)
+		})
+
+		it('Should return 400 when first name or last name is missing', async () => {
+			// Arrange
+			const {email, passport} = existingUser
+			const {password} = passport
+
+			// Action
+			const result = await apiRequest
+				.post('/auth/signup')
+				.send({email, passport, password})
+
+			const {status} = result.body
+			// Expect
+			expect(status).toEqual(httpStatus.BAD_REQUEST)
+		})
+
+		it('Should return 400 when user signs up with status', async () => {
+			// Arrange
+			const {email, passport, firstName, lastName, status} = existingUser
+			const {password} = passport
+
+			// Action
+			const result = await apiRequest
+				.post('/auth/signup')
+				.send({email, passport, password, firstName, lastName, status})
+
+			const {status: responseStatus} = result.body
+			// Expect
+			expect(responseStatus).toEqual(httpStatus.BAD_REQUEST)
 		})
 	})
 })
